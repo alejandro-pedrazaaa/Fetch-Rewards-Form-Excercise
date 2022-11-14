d1a7a4eb9f4c992b6a91cc8c878595971d1c7d67
Alejandro Pedraza <alepedpon1@gmail.com>
2022-11-14 01:07:31 -0600

    create navbar and footer that look like the ones Fetch Rewards uses

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("connnected");
+/**
+ * @description - When the user scrolls the page, the header resizes smoothly
+ */
+const headerContainer = document.querySelector(".header-container");
+window.onscroll = () => {
+  if (window.scrollY > 10) {
+    headerContainer.classList.remove("py-md-3");
+  } else {
+    headerContainer.classList.add("py-md-3");
+  }
+};
