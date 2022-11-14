1dcac1e4dec5cc84851fd9fe8238ad67f0557d44
Alejandro Pedraza <alepedpon1@gmail.com>
2022-11-14 15:59:34 -0600

    save to test on actual phone

--- script.js
+++ script.js
@@ -4,8 +4,32 @@
 const headerContainer = document.querySelector(".header-container");
 window.onscroll = () => {
   if (window.scrollY > 10) {
-    headerContainer.classList.remove("py-md-3");
+    headerContainer.classList.remove("py-3");
   } else {
-    headerContainer.classList.add("py-md-3");
+    headerContainer.classList.add("py-3");
   }
 };
+
+/**
+ * @description - Asynchronously fetches the data from the API
+ *
+ * @returns {Promise} - The data from the API
+ */
+const getDataFromAPI = async () => {
+  const res = await fetch("https://frontend-take-home.fetchrewards.com/form");
+  const data = await res.json();
+
+  createAndDisplayOptions(data.occupations);
+};
+
+const selectContainer = document.querySelector(".form-select");
+const createAndDisplayOptions = (occupations) => {
+  occupations.forEach((occupation) => {
+    const option = document.createElement("option");
+    option.value = occupation;
+    option.textContent = occupation;
+    selectContainer.appendChild(option);
+  });
+};
+
+getDataFromAPI();
